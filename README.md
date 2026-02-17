e0f32b9f729c7459325840a9dab6e0d9979f82e3
Luke Henderson <personal@lukeh.us>
2026-02-17 13:41:56 -0800

    feat/lab1: lab1 complete

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CSE 141 Compiler
 
 Compiler for UC Merced CSE 141 class
+
+## Lab 01
+
+To run the scanner and test on a file, compile the Java classes:
+
+```sh
+cd java
+javac *.java
+```
+
+Then run the scanner on the file:
+
+```sh
+java Scanner <path/to/your/file>.c
+```
+
+The scanner will output the processed file as `<file_name>_gen.c` in the same directory as the original file.
--- java/RegularExpressions.java
+++ java/RegularExpressions.java
@@ -2,6 +2,8 @@
  * This class implements the regular expressions used to parse the input
  */
 
+import java.util.*;
+
 public class RegularExpressions {
 	
 	
@@ -36,7 +38,7 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a digit
 	 */
 	private boolean isDigit(char c) {
-		// TODO: Implement the recognition of digits 
+		return Character.isDigit(c);
 	}
 	
 	/**
@@ -45,8 +47,20 @@ public class RegularExpressions {
 	 * @param str The input string that is being pasrsed which will be read one charecter at a time
 	 * @return boolean indicating if it is an identifier
 	 */
-	public boolean isIdentifier(String str) {		
-		// TODO: Implement the recognition of identifiers 
+	public boolean isIdentifier(String str) {
+		if (str.length() < 1) {
+			return false;
+		}
+		if (!Character.isLetter(str.charAt(0)) && str.charAt(0) != '_') {
+			return false;
+		}
+		for (int i = 1; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (!Character.isLetterOrDigit(c) && c != '_') {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/***
@@ -56,7 +70,15 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a number
 	 */
 	public boolean isNumber(String str) {
-		// TODO: Implement the recognition of numbers 
+		if (str.length() < 1) {
+			return false;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	/***
@@ -66,7 +88,16 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a reserved word
 	 */
 	public boolean isReservedWord(String str) {
-		// TODO: Implement the recognition of reserved words
+		if (str.length() < 1) {
+			return false;
+		}
+		// TODO: don't create new list, hashset every time
+		List<String> reserved_list = Arrays.asList(
+			"int", "void", "if", "while", "return", "read", "write", "print",
+			"continue", "break", "binary", "decimal"
+		);
+		Set<String> reserved_set = new HashSet<>(reserved_list);
+		return reserved_set.contains(str);
 	}
 	
 	/***
@@ -75,7 +106,35 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a symbol
 	 */
 	public boolean isSymbol(String str) {
-		// TODO: Implement the recognition of symbols
+		// why are bitwise operators not included? (& and |)
+		if (str.length() < 1) {
+			return false;
+		} else if (str.length() > 2) {
+			// all symbols are <= two chars
+			return false;
+		} else if (str.length() == 1) {
+			// optimistically assume most performant case
+			// handle single-character symbols
+			// TODO: don't create 
+			char[] symbol_chars = {
+				'(', ')', '[', ']', '{', '}', ',', ';', '+', '-', '*', '/', '=', '>',
+				'<'
+			};
+			char target = str.charAt(0);
+			for (char c : symbol_chars) {
+				if (c == target) {
+					return true;
+				}
+			}
+			return false;
+		} else {
+			// TODO: don't create new list, hashset every time.
+			List<String> multichar_symbols_list = Arrays.asList(
+				"==", "!=", ">=", "<=", "&&", "||"
+			);
+			Set<String> multichar_symbols_set = new HashSet<>(multichar_symbols_list);
+			return multichar_symbols_set.contains(str);
+		}
 	}
 	
 	/**
@@ -85,7 +144,16 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a string
 	 */
 	public boolean isString(String str) {
-		// TODO: Implement the recognition of strings
+		if (str.length() < 2) {
+			return false;
+		}
+		if (str.charAt(0) != '"') {
+			return false;
+		}
+		if (str.charAt(str.length() - 1) != '"') {
+			return false;
+		}
+		return true;
 	}
 	
 	/**
@@ -94,7 +162,20 @@ public class RegularExpressions {
 	 * @return boolean indicating if it is a meta statement
 	 */
 	public boolean isMetaStatement(String str) {
-		// TODO: Implement the recognition of meta statements
+		if (str.length() < 2) {
+			// if of length 1, cannot contain # and newline
+			return false;
+		}
+		// if (str.charAt(str.length() - 1) != '\n') {
+		// 	return false;
+		// }
+		if (str.charAt(0) == '#') {
+			return true;
+		}
+		if (str.charAt(0) == '/' && str.charAt(1) == '/') {
+			return true;
+		}
+		return false;
 	}
 
 }
